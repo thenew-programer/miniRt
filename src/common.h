d848c62be717ff6fd984f6dc12f6f05402b7b270
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-08-17 22:04:04 +0100

    common.h header file for common libraries used in all models

---
+++ src/common.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   common.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybouryal <ybouryal@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/17 10:19:43 by ybouryal          #+#    #+#             */
+/*   Updated: 2025/08/17 12:27:47 by ybouryal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMON_H
+# define COMMON_H
+
+# include <stdio.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <string.h>
+# include "../libft/libft.h"
+
+#endif /* COMMON_H */
